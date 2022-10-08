723f706de266457d7501fcefa93dcb783c639151
Yevhen Bala <109308300+LukeWesker@users.noreply.github.com>
2022-10-08 16:30:49 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        infinite: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+  });
+});
